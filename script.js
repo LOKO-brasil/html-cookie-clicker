c944901dacde1321601b6af291998869abf5f406
Pem010 <89361979+Pem010@users.noreply.github.com>
2021-08-22 20:11:43 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+				var clicks= 0;
+				var score= document.getElementById("score");
+				var cookie= document.getElementById("cookie");
+				function cookie_click(){
+				clicks++;
+				score.innerHTML = ("Score: " + clicks);
+				}
+				function dev_cheat(){
+								clicks= 999;
+				}
+if(clicks > 1000) {
+				cookie.innerHTML = ("&#128142;");
+				}
