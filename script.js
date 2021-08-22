acfa2739767d32f795d94e085b2cf1dbf05d156f
Pem010 <89361979+Pem010@users.noreply.github.com>
2021-08-22 20:16:53 -0300

    Adjust of code

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-				var clicks= 0;
-				var score= document.getElementById("score");
-				var cookie= document.getElementById("cookie");
-				function cookie_click(){
-				clicks++;
-				score.innerHTML = ("Score: " + clicks);
-				}
-				function dev_cheat(){
+var clicks= 0;
+var score= document.getElementById("score");
+var cookie= document.getElementById("cookie");
+function cookie_click(){
+clicks++;
+score.innerHTML = ("Score: " + clicks);
+}
+function dev_cheat(){
 								clicks= 999;
-				}
+}
 if(clicks > 1000) {
-				cookie.innerHTML = ("&#128142;");
-				}
+cookie.innerHTML = ("&#128142;");
+}
